2564da88dc09f8c2fa50840b5d5e65a656385822
Rodrigo_Torres <torresc.rodri@gmail.com>
2021-08-30 10:01:09 -0300

    version 3

---
+++ Carpeta Desarrollador B/desarrolloB.java
@@ -0,0 +1,6 @@
+public class desarrolloB {
+    public static void main() {
+        System.out.println("Desarrollo B 30/8/2021 09:58"); // imprimir
+
+    }
+}
---
+++ carpeta Desarrollador A/desarrolloA.java
@@ -0,0 +1,6 @@
+public class desarrolloA {
+    public static void main() {
+        System.out.println("Desarrollo A 30/8/2021 09:59"); // imprimir
+
+    }
+}
