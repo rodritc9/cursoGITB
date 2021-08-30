a1fb749b119582dfdc0a074cb04d7dc8c952a175
Rodrigo_Torres <torresc.rodri@gmail.com>
2021-08-30 14:51:52 -0300

    Desarrollo desde Rama Desarrollador B

--- carpeta Desarrollador A/desarrolloA.java
+++ carpeta Desarrollador A/desarrolloA.java
@@ -1,3 +1,4 @@
+
 public class desarrolloA {
     public static void main() {
         System.out.println("Desarrollo A 30/8/2021 09:59"); // imprimir
---
+++ desarrolladorBNuevo.java
@@ -0,0 +1,6 @@
+public class desarrolladorBNuevo {
+    public static void main() {
+        System.out.println("Archivo nuevo desde rama de desarrollador B 30/8/2021 14:50"); 
+
+    }
+}
